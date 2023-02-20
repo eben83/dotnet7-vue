1dccc51dfe7b2e26043ce4f2ebe8d356bb2247f5
Eben Burger <eben@therewardsfactory.com>
2023-02-20 20:28:55 +0100

    models added

---
+++ dontnet7-vue-auth/dontnet7-vue-auth.api/Models/User.cs
@@ -0,0 +1,9 @@
+namespace dontnet7_vue_auth.api.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
+}
